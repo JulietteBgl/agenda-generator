5a33c8b31d0475c967a666e647fa1229e707e149
juliette.bassnagel <juliette.bassnagel@decathlon.com>
2025-07-20 21:34:31 +0200

    [REF] remove to_csv function

--- utils/export_agenda.py
+++ utils/export_agenda.py
@@ -7,6 +7,3 @@ def to_excel(df):
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False)
     return buffer.getvalue()
-
-def to_csv(df):
-    return df.to_csv(index=False).encode('utf-8', errors='ignore')
